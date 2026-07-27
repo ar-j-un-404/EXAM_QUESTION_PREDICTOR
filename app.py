55198d52192970fc2bcd8f5ae0d07efb143699e7
ar-j-un-404 <arjun127002@gmail.com>
2026-07-27 19:00:48 +0530

    Initial commit: AI Exam Question Predictor

---
+++ app.py
@@ -0,0 +1,56 @@
+from sentence_transformers import SentenceTransformer
+from sentence_transformers.util import cos_sim
+import os
+import re
+from pypdf import PdfReader
+
+model=SentenceTransformer("all-MiniLM-L6-v2")
+
+files=os.listdir("papers")
+
+questions=[]
+for file in files:
+    if file.endswith(".pdf"):
+       pdf_path=os.path.join("papers",file)
+       pdf_read=PdfReader(pdf_path)
+
+       for page in pdf_read.pages:
+         text= page.extract_text()
+         if text is not None:
+            lines=text.split("\n")
+
+            for line in lines:
+               line=line.strip()
+               line = line.strip()
+
+               if re.match(r"^\d+[.)]", line):
+                   line = re.sub(r"^\d+[.)]\s*", "", line)
+                   questions.append(line)               
+         
+
+frequency={}
+embedding={}
+for question in questions:
+   embedding[question]=model.encode(question)
+   
+for question in questions:
+   match = False
+   for existing_question in frequency:
+      similarity=cos_sim(embedding[question],embedding[existing_question]).item()
+
+      
+
+      if similarity>0.80:
+         frequency[existing_question]+=1
+         match =True
+         break
+   if not match:
+         frequency[question]=1
+sorted_questions=sorted(frequency.items(),key=lambda item:item[1],reverse=True)
+
+for question, count in sorted_questions:
+   print(question,"->",count)
+
+
+
+
